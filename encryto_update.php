126b57049214ca28e8ae37de4961538c7f587612
Sketify <78920165+Sketify@users.noreply.github.com>
2021-04-10 00:24:54 +0530

    main_commit

---
+++ encryto_update.php
@@ -0,0 +1,25 @@
+From: <Saved by Blink>
+Snapshot-Content-Location: http://encryto.000webhostapp.com/update.php
+Subject: 
+Date: Wed, 31 Mar 2021 09:53:21 -0000
+MIME-Version: 1.0
+Content-Type: multipart/related;
+	type="text/html";
+	boundary="----MultipartBoundary--vC4cjW5scnVWNPMhejoTQcSrCcgWHQEBMEvPBSjlFl----"
+
+
+------MultipartBoundary--vC4cjW5scnVWNPMhejoTQcSrCcgWHQEBMEvPBSjlFl----
+Content-Type: text/html
+Content-ID: <frame-73258C126D2B28D2F5F3D879D5C19C71@mhtml.blink>
+Content-Transfer-Encoding: binary
+Content-Location: http://encryto.000webhostapp.com/update.php
+
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8"></head><body><br>
+<b>Warning</b>:  mysqli_connect(): (HY000/1045): ProxySQL Error: Access denied for user 'id16411225_mskdev'@'2a02:4780:bad:f00d::6' (using password: YES) in <b>/storage/ssd3/241/16500241/public_html/update.php</b> on line <b>18</b><br>
+<br>
+<b>Notice</b>:  Trying to get property 'connect_error' of non-object in <b>/storage/ssd3/241/16500241/public_html/update.php</b> on line <b>22</b><br>
+Connected successfully
+<br>
+<b>Notice</b>:  Undefined index: type in <b>/storage/ssd3/241/16500241/public_html/update.php</b> on line <b>35</b><br>
+</body></html>
+------MultipartBoundary--vC4cjW5scnVWNPMhejoTQcSrCcgWHQEBMEvPBSjlFl------
