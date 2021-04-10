7c38eb7ffeeae3c92308996d02ef033e6f95fed8
Sketify <78920165+Sketify@users.noreply.github.com>
2021-04-10 09:43:23 +0530

    Update encrypto_login.php

--- api/v2/auth/encrypto_login.php
+++ api/v2/auth/encrypto_login.php
@@ -1,1 +1,50 @@
-<?php include ('connection1.php');   if($con->connect_error){              echo "Connection failed: ". $con->connect_error;           } else {                 $email = $_POST["email"];$password = $_POST["pass"];  $sql = "select username, id from users where email = '$email' and password = '$password'";  $result = mysqli_query($con,$sql);  if(mysqli_num_rows($result)<0){ $status = "failed";echo json_encode(array("response"=>$status));  } else{ $row = mysqli_fetch_assoc($result);$name = $row['username'];$Id = $row['id'];$status = "ok"; echo json_encode(array("response"=>$status,"email"=>$email,"name"=>$name,"id"=>$Id));   }  mysqli_close($con);             }?>
+<?php
+
+ include ('connection.php');
+ 
+  if($con->connect_error){
+              
+echo "Connection failed: ". $con->connect_error;
+
+           } else {
+
+                
+ 
+$email = $_POST["email"];
+$password = $_POST["pass"];
+ 
+ $sql = "select username, id from users where email = '$email' and password = '$password'";
+ 
+ $result = mysqli_query($con,$sql);
+ 
+ if(mysqli_num_rows($result)<0){
+ 
+$status = "failed";
+echo json_encode(array("response"=>$status));
+ 
+ }
+ else{
+ 
+$row = mysqli_fetch_assoc($result);
+$name = $row['username'];
+
+$Id = $row['id'];
+$status = "ok";
+
+ echo json_encode(array("response"=>$status,"email"=>$email,"name"=>$name,"id"=>$Id));
+ 
+ 
+ }
+ 
+ mysqli_close($con);
+ 
+ 
+           }
+
+?>
+
+
+
+
+
+
